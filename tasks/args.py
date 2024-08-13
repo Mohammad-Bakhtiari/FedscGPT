e06653686a20ede3714eb6914705e066f682eb1d
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-08-13 11:30:14 +0200

    exp with init weights

--- tasks/args.py
+++ tasks/args.py
@@ -14,12 +14,13 @@ def instantiate_args():
     parser.add_argument('--reference_adata', type=str, default='ms.h5ad')
     parser.add_argument('--query_adata', type=str, default='ms.h5ad')
     parser.add_argument('--pretrained_model_dir', type=str, default=f'{HOME_DIR}/pretrained_models/scGPT_human')
+    parser.add_argument("--init_weights_dir", type=str, default=f"{HOME_DIR}/init_weights/hp.pth")
     parser.add_argument("--finetune_model_dir", type=str)
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--config_file', type=str, help='.yml file for the model', default='config.yml')
     parser.add_argument("--verbose", action='store_true', default=False)
     parser.add_argument("--dataset_name", type=str, default='ms')
-    parser.add_argument("--shared_res_file", type=str, default=f"{HOME_DIR}/output/annotation/shared_res.csv")
+    parser.add_argument("--param_tuning_res", type=str, default=f"{HOME_DIR}/output/annotation/param-tuning_res")
     return parser
 
 
