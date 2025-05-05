66e085ff293ab04edaf06da8b103e2073d6a0f4a
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-05-05 15:40:21 +0200

    fed client

---
+++ FedscGPT/federated/client.py
@@ -0,0 +1,49 @@
+from FedscGPT.centralized import Training
+from FedscGPT.utils import check_weights_nan
+
+class Client(Training):
+    def __init__(self, n_total_samples, smpc=False, debug=False, **kwargs):
+        super().__init__(**kwargs)
+        self.smpc = smpc
+        self.n_samples = self.adata.X.shape[0]
+        self.sample_ration = self.n_samples / n_total_samples
+        self.debug = debug
+
+    def get_local_updates(self):
+        """Get the local updates.
+
+        Returns
+        -------
+        dict, int : Without SMPC
+        list of crypten.cryptensor, crypten.cryptensor : With SMPC
+        """
+        if self.smpc:
+            weights = self.get_weights().values()
+            if self.aggregation == "weighted_fedavg":
+               weights = [param * self.sample_ration for param in weights]
+            check_weights_nan(weights, "after training", self.debug)
+            encrypted_weights = [crypten.cryptensor(param) for param in weights]
+            return encrypted_weights
+
+        return self.get_weights(), self.n_samples
+
+    def get_weights(self):
+        """ Get the weights of the model
+        Returns
+        -------
+        dict
+            The weights of the model
+        """
+        return self.model.state_dict()
+
+    def set_weights(self, state_dict):
+        """ Set the weights of the model
+        Parameters
+        ----------
+        state_dict: dict
+            The weights of the model
+        """
+        with torch.no_grad():
+            for name, param in self.model.named_parameters():
+                if name in state_dict:
+                    param.data.copy_(state_dict[name].to(param.device))
