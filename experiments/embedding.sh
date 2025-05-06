8e47fc6ebdf4f23052b9b1fa46ddfc8c1daac4bc
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-05-06 16:06:40 +0200

    smpc

--- experiments/embedding.sh
+++ experiments/embedding.sh
@@ -9,6 +9,9 @@ celltype_key="$5"
 batch_key="$6"
 gene_col="$7"
 gpu="$8"
+smpc="${9-flase}"
+
+echo "Args: mode: $mode, dataset: $dataset, reference_file: $reference_file, query_file: $query_file, celltype_key: $celltype_key, batch_key: $batch_key, gene_col: $gene_col, gpu: $gpu"
 
 # Get the root directory, which is the parent directory of the current working directory
 root_dir="$(dirname "$PWD")"
@@ -29,7 +32,7 @@ if [ ! -d "$output" ]; then
 fi
 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
-python ${root_dir}/tasks/embedding.py \
+cmd="python ${root_dir}/tasks/embedding.py \
  --dataset_name $dataset \
  --data-dir $data_dir \
  --reference_adata $reference \
@@ -40,6 +43,13 @@ python ${root_dir}/tasks/embedding.py \
  --gene_col "$gene_col" \
  --mode $mode \
  --gpu $gpu \
- --pretrained_model_dir "${root_dir}/models/pretrained_models/scGPT_human" \
- --config_file "${root_dir}/experiments/configs/embedding/config.yml" \
- --fed_config_file "${root_dir}/experiments/configs/embedding/fed_config.yml"
+ --pretrained_model_dir ${root_dir}/models/pretrained_models/scGPT_human \
+ --config_file ${root_dir}/experiments/configs/embedding/config.yml \
+ --fed_config_file ${root_dir}/experiments/configs/embedding/fed_config.yml"
+
+if [ "$smpc" == "true" ]; then
+    cmd="$cmd --smpc"
+fi
+
+# Execute the command
+eval $cmd
