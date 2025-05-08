2dc5fa10ae3c592d4655fd6a50ef2709ee998186
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-05-08 17:35:39 +0200

    debug

--- FedscGPT/utils.py
+++ FedscGPT/utils.py
@@ -1075,7 +1075,6 @@ def suppress_argmin(dist_matrix, argmin_onehot, batch_size=128, large_val=1e9):
 
 def top_k_encrypted_distances(encrypted_dist_matrix, k):
     topk_indices = top_k_ind_selection(encrypted_dist_matrix.clone(), k)
-    import pdb; pdb.set_trace()
     encrypted_topk = (encrypted_dist_matrix * topk_indices[0]).sum(dim=1, keepdim=True)
     for i in range(1, k):
         next_k = (encrypted_dist_matrix * topk_indices[i]).sum(dim=1, keepdim=True)
