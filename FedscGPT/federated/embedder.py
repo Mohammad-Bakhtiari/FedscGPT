c4fb3f487b87ad3b199a7065c35d960277240dd7
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-05-13 15:28:53 +0200

    ind + 1

--- FedscGPT/federated/embedder.py
+++ FedscGPT/federated/embedder.py
@@ -375,8 +375,8 @@ class FedEmbedder(FedBase):
             class_range = torch.arange(self.n_classes, dtype=torch.long, device=self.device).view(1, -1)
             enc_one_hot = (flat_indices == class_range).view(self.n_query_samples, self.k, self.n_classes)
             _, arg_max = enc_one_hot.sum(dim=1).max(dim=1)
-            pred_labels = arg_max.get_plain_text().argmax(dim=1).cpu().numpy().astype('int')
-            import pdb; pdb.set_trace()
+            pred_labels = arg_max.get_plain_text().argmax(dim=1).cpu().numpy().astype('int') + 1
+            # import pdb; pdb.set_trace()
             pred_labels_plain = np.array([self.index_to_label[label] for label in pred_labels], dtype=object)
             return pred_labels_plain
 
