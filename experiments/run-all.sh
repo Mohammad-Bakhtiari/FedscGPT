f41ae57ada67d055cfd7c49058b3cf0989e13e07
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-05-15 16:35:07 +0200

    exp finalized

--- experiments/run-all.sh
+++ experiments/run-all.sh
@@ -24,20 +24,20 @@ echo -e "\e[34m-------------------------------------\e[0m"
 ./run_annotation.sh centralized_clients 20 0 false $GPU
 
 chmod +x annotation.sh
-echo -e "\e[34m-------------------------------------\e[0m"
-echo -e "\e[34m Running annotation with FedscGPT without SMPC on MS dataset for two local epochs and seven rounds\e[0m"
-echo -e "\e[34m-------------------------------------\e[0m"
-./annotation.sh federated_finetune ms reference.h5ad query.h5ad "Factor Value[inferred cell type - authors labels]" "Factor Value[sampling site]" $GPU 2 7 false
-
-echo -e "\e[34m-------------------------------------\e[0m"
-echo -e "\e[34m Running annotation with FedscGPT without SMPC on HP dataset for one local epoch and three rounds\e[0m"
-echo -e "\e[34m-------------------------------------\e[0m"
-./annotation.sh federated_finetune hp reference_refined.h5ad query.h5ad Celltype batch $GPU 1 3 false
-
-echo -e "\e[34m-------------------------------------\e[0m"
-echo -e "\e[34m Running annotation with FedscGPT without SMPC on Myeloid dataset for four local epochs and one round\e[0m"
-echo -e "\e[34m-------------------------------------\e[0m"
-./annotation.sh federated_finetune myeloid reference_adata.h5ad query_adata.h5ad combined_celltypes top4+rest $GPU 4 1 false
+#echo -e "\e[34m-------------------------------------\e[0m"
+#echo -e "\e[34m Running annotation with FedscGPT without SMPC on MS dataset for two local epochs and seven rounds\e[0m"
+#echo -e "\e[34m-------------------------------------\e[0m"
+#./annotation.sh federated_finetune ms reference.h5ad query.h5ad "Factor Value[inferred cell type - authors labels]" "Factor Value[sampling site]" $GPU 2 7 false
+#
+#echo -e "\e[34m-------------------------------------\e[0m"
+#echo -e "\e[34m Running annotation with FedscGPT without SMPC on HP dataset for one local epoch and three rounds\e[0m"
+#echo -e "\e[34m-------------------------------------\e[0m"
+#./annotation.sh federated_finetune hp reference_refined.h5ad query.h5ad Celltype batch $GPU 1 3 false
+#
+#echo -e "\e[34m-------------------------------------\e[0m"
+#echo -e "\e[34m Running annotation with FedscGPT without SMPC on Myeloid dataset for four local epochs and one round\e[0m"
+#echo -e "\e[34m-------------------------------------\e[0m"
+#./annotation.sh federated_finetune myeloid reference_adata.h5ad query_adata.h5ad combined_celltypes top4+rest $GPU 4 1 false
 
 
 chmod +x run_embedding.sh
