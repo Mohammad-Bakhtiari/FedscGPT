508b33b2ceaf4b78d73543afd0ab90df2d7c5fe2
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-06-12 15:20:58 +0200

    new ds dir

--- experiments/run_annotation.sh
+++ experiments/run_annotation.sh
@@ -17,8 +17,8 @@ datasets["MS"]="ms|reference.h5ad|query.h5ad|Factor Value[inferred cell type - a
 datasets["HP"]="hp|reference_refined.h5ad|query.h5ad|Celltype|batch"
 datasets["MYELOID-top4+rest"]="myeloid|reference_adata.h5ad|query_adata.h5ad|combined_celltypes|top4+rest"
 datasets["COVID"]="covid|reference_annot.h5ad|query_annot.h5ad|celltype|batch_group"
-datasets["COVID-cent_corrected"]="covid|reference_corrected.h5ad|query_corrected.h5ad|celltype|batch_group"
-datasets["COVID-fed-corrected"]="covid|reference_fed_corrected.h5ad|query_fed_corrected.h5ad|celltype|batch_group"
+datasets["COVID-cent_corrected"]="covid-corrected|reference.h5ad|query.h5ad|celltype|batch_group"
+datasets["COVID-fed-corrected"]="covid-fed-corrected|reference.h5ad|query.h5ad|celltype|batch_group"
 datasets["LUNG"]="lung|reference_annot.h5ad|query_annot.h5ad|cell_type|sample"
 datasets["CellLine"]="cl|reference.h5ad|query.h5ad|cell_type|batch"
 
