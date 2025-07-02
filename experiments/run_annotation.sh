bdaf0e32307e2ec45d9114275996745e901bb131
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-07-02 10:00:48 +0200

    flex ds

--- experiments/run_annotation.sh
+++ experiments/run_annotation.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Assign command-line arguments to variables
-mode="$1"
-n_epochs="$2"
-n_rounds="$3"
-smpc="${4-false}"
-GPU="${5-0}"
-agg_method="${6-fedavg}"
-weighted="${7-false}"
-mu="${8-0}"
+datasetnames="${1}"
+mode="$2"
+n_epochs="$3"
+n_rounds="$4"
+smpc="${5-false}"
+GPU="${6-0}"
+agg_method="${7-fedavg}"
+weighted="${8-false}"
+mu="${9-0}"
 
 chmod +x annotation.sh
 
 declare -A datasets
 datasets["MS"]="ms|reference.h5ad|query.h5ad|Factor Value[inferred cell type - authors labels]|Factor Value[sampling site]"
+datasets["MS-refined"]="refined_ms|reference-raw.h5ad|query-raw.h5ad|Factor Value[inferred cell type - authors labels]|Factor Value[sampling site]"
 datasets["HP"]="hp|reference_refined.h5ad|query.h5ad|Celltype|batch"
 datasets["MYELOID-top4+rest"]="myeloid|reference_adata.h5ad|query_adata.h5ad|combined_celltypes|top4+rest"
 datasets["COVID"]="covid|reference_annot.h5ad|query_annot.h5ad|celltype|batch_group"
@@ -22,11 +24,23 @@ datasets["COVID-fed-corrected"]="covid-fed-corrected|reference.h5ad|query.h5ad|c
 datasets["LUNG"]="lung|reference_annot.h5ad|query_annot.h5ad|cell_type|sample"
 datasets["CellLine"]="cl|reference.h5ad|query.h5ad|cell_type|batch"
 
+IFS=',' read -ra keys <<< "$datasetnames"
+if [[ "${datasetnames}" != "all" ]]; then
+    for key in "${keys[@]}"; do
+        if [[ -z "${datasets[$key]}" ]]; then
+            echo "Dataset \"$key\" not found. Available keys: ${!datasets[@]}"
+            exit 1
+        fi
+    done
+else
+    keys=("${!datasets[@]}")
+fi
+
 
 echo "Running annotation for ${mode}"
 
 
-for key in "${!datasets[@]}"; do
+for key in "${keys[@]}"; do
     echo -e "\e[32m******************************************\e[0m"
     echo -e "\e[32mRunning annotation for $key dataset [SMPC is ${smpc}]\e[0m"
     IFS='|' read -r -a args <<< "${datasets[$key]}"
