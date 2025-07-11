3d9f52412954f9692fec9ef6db128f27f303fd57
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-07-11 15:16:05 +0200

    filenames corr

--- experiments/configs.sh
+++ experiments/configs.sh
@@ -6,7 +6,7 @@ datasets["HP"]="hp|reference_refined.h5ad|query.h5ad|Celltype|batch"
 datasets["MYELOID-top4+rest"]="myeloid|reference_adata.h5ad|query_adata.h5ad|combined_celltypes|top4+rest"
 datasets["LUNG"]="lung|reference_annot.h5ad|query_annot.h5ad|cell_type|sample"
 datasets["CellLine"]="cl|reference.h5ad|query.h5ad|cell_type|batch"
-datasets["COVID"]="covid|reference_raw.h5ad|query_raw.h5ad|celltype|batch_group"
+datasets["COVID"]="covid|reference-raw.h5ad|query-raw.h5ad|celltype|batch_group"
 datasets["COVID-cent_corrected"]="covid-corrected|reference_corrected.h5ad|query_corrected.h5ad|celltype|batch_group"
 datasets["COVID-fed-corrected"]="covid-fed-corrected|reference_fed_corrected.h5ad|query_fed_corrected.h5ad|celltype|batch_group"
 
